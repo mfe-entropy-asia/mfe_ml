c7ad65340b7ced915785c7fc495467ed8e373445
yi liu <liuyi92815@gmail.com>
2019-02-28 23:52:55 +0800

    Put the re.compile in the initial block so it will be executed only once when instantiated.

--- data_cleaner.py
+++ data_cleaner.py
@@ -15,6 +15,9 @@ class DataCleaner:
         self.language = language
         self.input_file_lst = input_file_lst
         self.output_file = output_file
+        self.find_headline = re.compile('\"headline\": \"(.*?)\"')
+        self.find_body = re.compile('\"body\": \"(.*?)\"')
+        self.find_data = re.compile("\"data\": {(.*?)}")
 
     def __call__(self):
         self.remove_out_file()  # Remove the output file if it exists
@@ -23,29 +26,26 @@ class DataCleaner:
         self.gen_body_lst()
 
     def gen_headline_lst(self):
-        find_headline = re.compile('\"headline\": \"(.*?)\"')
         with open(self.output_file, encoding="utf-8") as f:
             for line in f:
-                m_headline = find_headline.search(line)
+                m_headline = self.find_headline.search(line)
                 self.headline_lst.append(m_headline.group(1))   
 
     def gen_body_lst(self):
-        find_body = re.compile('\"body\": \"(.*?)\"')
         with open(self.output_file, encoding="utf-8") as f:
             for line in f:
-                m_body = find_body.search(line)
+                m_body = self.find_body.search(line)
                 self.body_lst.append(m_body.group(1))   
 
     def filter_language(self):
         # This function is to filter news in English language and to keep only the content of data section
-        find_data = re.compile("\"data\": {(.*?)}")
-        find_lg = re.compile("\"language\": \""+self.language+"\"")
+        find_lg = re.compile("\"language\": \"" + self.language + "\"")
         for input_file in self.input_file_lst:
             with open(input_file, encoding="utf-8") as f:
                 output = open(self.output_file, "a+", encoding="utf-8")
                 for line in f:
                     if find_lg.search(line):
-                        m_data = find_data.search(line)
+                        m_data = self.find_data.search(line)
                         output.write(m_data.group(1)+'\n')
             output.close()
 
