af125961a14aebf4be19cecd3754f0588de1c1a0
yi liu <liuyi92815@gmail.com>
2019-02-28 23:55:41 +0800

    A trial of first sentence tokenize and then word tokenize

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from nltk import sent_tokenize
 from nltk import Text
 dat_clean = DataCleaner("en", ["news.txt"], "en_output.dat")
 dat_clean()
-tokens = sent_tokenize(dat_clean.body_lst[4])
+tokens = [word_tokenize(sentence) for sentence in sent_tokenize(dat_clean.body_lst[4])]
 for i in tokens:
     print(i)
