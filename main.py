3a905ace444e79122c4ae9970e5b6684b75715c0
yi liu <liuyi92815@gmail.com>
2019-03-02 00:26:50 +0800

    Added a print of first 8 headlines

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from nltk import sent_tokenize
 from nltk import Text
 dat_clean = DataCleaner("en", ["news.txt"], "en_output.dat")
 dat_clean()
+for i in range(8):
+    print(dat_clean.headline_lst[i])
 tokens = [word_tokenize(sentence) for sentence in sent_tokenize(dat_clean.body_lst[4])]
 for i in tokens:
     print(i)
