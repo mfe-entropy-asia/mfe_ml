8c2beb57c32732b40aacdc020705a4799c8a4eb6
yi liu <liuyi92815@gmail.com>
2019-03-09 13:44:29 +0800

    Print out the value of the dictionary

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from data_cleaner import DataCleaner
-from nltk import ngrams
-from nltk import FreqDist
-from nltk import word_tokenize
-from nltk import sent_tokenize
-from nltk import Text
+# from nltk import ngrams
+# from nltk import FreqDist
+# from nltk import word_tokenize
+# from nltk import sent_tokenize
+# from nltk import Text
 dat_clean = DataCleaner("en", ["news.txt"], "en_output.dat")
 dat_clean()
-for i in range(8):
-    print(dat_clean.headline_lst[i])
-tokens = [word_tokenize(sentence) for sentence in sent_tokenize(dat_clean.body_lst[4])]
-for i in tokens:
-    print(i)
+# for i in range(8):
+#     print(dat_clean.headline_lst[i])
+# tokens = [word_tokenize(sentence) for sentence in sent_tokenize(dat_clean.body_lst[4])]
+# for i in tokens:
+#     print(i)
+
+for key in dat_clean.m_dict:
+    print(dat_clean.m_dict[key][4])
