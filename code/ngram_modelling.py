7cd706c3f6505407b81d27e7d26c49730e14a933
yi liu <liuyi92815@gmail.com>
2019-03-16 16:32:47 +0800

    Some trial

--- code/ngram_modelling.py
+++ code/ngram_modelling.py
@@ -1,6 +1,6 @@
 import numpy as np
 from data_cleaner import DataCleaner
-# from nltk.util import ngrams
+from nltk.util import ngrams
 # from nltk import FreqDist
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
@@ -13,25 +13,30 @@ translator = str.maketrans('', '', string.punctuation)  # To get rid of the punc
 dat_clean = DataCleaner("en", ["./data/raw/News.RTRS.201806.0214.txt"], "./data/intermediate/")
 dat_clean()
 
-# print(dat_clean.body_lst[4])
-# print(sent_tokenize(dat_clean.body_lst[4]))
 text = []
-for body in dat_clean.m_dict[np.datetime64('2018-06-01')]:
-    # if body != '':
-    text += [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(body)]
+for i in range(3):
+    for body in dat_clean.m_dict[np.datetime64('2018-06-01')+i]:
+        # if body != '':
+        text += [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(body)]
 
-# tokens = [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(dat_clean.body_lst[4])]
-# print(tokens)
-# for i in tokens:
-#     print(i)
+date = np.datetime64('2018-06-01') + 4
+present_day_text = []
+for body in dat_clean.m_dict[date]:
+    present_day_text += [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(body)]
+text_fourgram = [ngrams(sent, 4) for sent in present_day_text]
+
 train, vocab = padded_everygram_pipeline(4, text)  # This will generate unigram, bigram, trigram and fourgram
+
+
 lm = MLE(4)
 lm.fit(train, vocab)
-# for i in lm.vocab:
-#     print(i)
+# 'verdascos', 'early', 'charge'), FreqDist({'he': 1})
 
-# for i in lm.counts[4]:  # print all the 3 grams
-#     print(i)
 
-print(len(lm.vocab))
-print(lm.counts)
+# a = sorted(lm.counts[4].items())
+# print(a[::-400])
+for i in lm.counts[4]:  # print all the 3 grams
+    print(i)
+print(lm.score('</s>', ['your', 'scheduler', '</s>']))
+# print(len(lm.vocab))
+# print(lm.counts)
