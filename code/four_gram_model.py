55bebac1e9b475e00042b56af2bcfb3f5886ba4a
yi liu <liuyi92815@gmail.com>
2019-03-17 23:16:32 +0800

    Move the message printing to within the 4gram model. It now trains with only three gram and four gram.
    Train and store module architecture modification

--- code/four_gram_model.py
+++ code/four_gram_model.py
@@ -8,10 +8,11 @@ from nltk.lm.preprocessing import padded_everygram_pipeline
 from nltk.lm import MLE
 import string
 import pickle
+import time
 # from nltk import Text
 # from data_cleaner import DataCleaner
 # from nltk.util import pad_sequence
-# from nltk.util import ngrams
+from nltk.util import ngrams
 # from nltk import FreqDist
 
 
@@ -34,7 +35,16 @@ class FourGramModel:
         return
 
     def __call__(self):
+        start = time.time()
+        print("=====================================\n" +
+              "Fitting model for date: " + str(self.input_date) +
+              "...\n=====================================\n")
         self.train_model()
+        end = time.time()
+        print("===============================================\n" +
+              "Model for date: " + str(self.input_date) + " has been fitted!!!!!! \n" +
+              "Time taking: " + str(end-start) + "\n"
+              "===============================================\n")
 
     def train_model(self):
         """
@@ -47,8 +57,13 @@ class FourGramModel:
             for content in self.input_dict[self.input_date - idx - 1]:
                 training_text += [word_tokenize(sentence.translate(self.translator))
                                   for sentence in sent_tokenize(content)]
-        self.train, self.vocab = padded_everygram_pipeline(4, training_text)
-        self.lm.fit(self.train, self.vocab)
+        # self.train, self.vocab = padded_everygram_pipeline(4, training_text)
+        every_gram, vocab = padded_everygram_pipeline(4, training_text)  # This will generate unigram, bigram,
+        # trigram and fourgram
+        three_gram = [item for sent in list(every_gram) for item in sent if len(item) == 3]
+        four_gram = [item for sent in list(every_gram) for item in sent if len(item) == 4]
+        train = [three_gram, four_gram]
+        self.lm.fit(train, vocab)
 
 
 #  Only executed when this file is called directly, this part of code is for debug purpose only
@@ -56,30 +71,38 @@ if __name__ == '__main__':
     translator = str.maketrans('', '', string.punctuation)  # To get rid of the punctuations
     pickle_in = open("./data/intermediate/dict.pickle", "rb")
     processed_news_dict = pickle.load(pickle_in)
-    # model_1 = FourGramModel(processed_news_dict, np.datetime64('2018-06-08'))
-    # model_1()
-    #
-    # pickle_out = open("./data/intermediate/model.pickle", "wb")
-    # pickle.dump(model_1.lm, pickle_out)
-    # pickle_out.close()
+    model_1 = FourGramModel(processed_news_dict, np.datetime64('2018-06-08'))
+    model_1()
+    pickle_out = open("./data/intermediate/model.pickle", "wb")
+    pickle.dump(model_1.lm, pickle_out)
+    pickle_out.close()
     # a = sorted(model_1.lm.counts[3].items())
     # print(a[::-400])
-    text = []
-    for i in range(40):
-        for body in processed_news_dict[np.datetime64('2018-06-01')+i]:
-            # if body != '':
-            text += [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(body)]
+    # text = []
+    # for i in range(40):
+    #     for body in processed_news_dict[np.datetime64('2018-06-01')+i]:
+    #         # if body != '':
+    #         text += [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(body)]
 
     # date = np.datetime64('2018-06-01') + 4
     # present_day_text = []
     # for body in processed_news_dict[date]:
     #     present_day_text += [word_tokenize(sentence.translate(translator)) for sentence in sent_tokenize(body)]
     # text_fourgram = [ngrams(sent, 4) for sent in present_day_text]
-    train, vocab = padded_everygram_pipeline(4, text)  # This will generate unigram, bigram, trigram and fourgram
-    lm = MLE(4)
-    lm.fit(train, vocab)
+    # for i in text_fourgram:
+    #     print(list(i))
+    # every_gram, vocab = padded_everygram_pipeline(4, present_day_text)  # This will generate unigram, bigram,
+    # # trigram and fourgram
+    # three_gram = [item for sent in list(every_gram) for item in sent if len(item) == 3]
+    # four_gram = [item for sent in list(every_gram) for item in sent if len(item) == 4]
+    # train = [three_gram, four_gram]
+    # for i in train:
+    #     print(i)
+        # for item in list(i):
+        #     print(item)
+    # lm = MLE(4)
+    # lm.fit(train, vocab)
     # a = sorted(lm.counts[4].items())
-    # print(a[::-400])
     # for i in lm.counts[4]:  # print all the 3 grams
     #     print(i)
     # print(lm.score('almost', ['yields', 'have', 'fallen']))
--- code/train_and_store_model.py
+++ code/train_and_store_model.py
@@ -1,8 +1,9 @@
-from FourGramModel import FourGramModel
+from four_gram_model import FourGramModel
 from data_cleaner import DataCleaner
 import pickle
 import numpy as np
 import os
+import time
 from multiprocessing import Pool, Queue
 
 
@@ -25,30 +26,28 @@ def multiple_processing_func(input_date):
     if input_date < np.datetime64('2018-06-08'):
         return
     else:
-        print("=====================================\n" + "Fitting model for date: " + str(input_date) +
-              "...\n=====================================")
-        fitted_models[input_date] = FourGramModel(processed_news_dict, input_date)
+        model = FourGramModel(processed_news_dict, input_date)
         try:
-            fitted_models[input_date]()
+            model()
+            fitted_models[input_date] = model.lm
         except Exception as e:
             print("Training failed;" + e)
 
-        print("=====================================\n""Model for date: " + str(input_date) +
-              " has been fitted!!!!!! \n" + "=====================================")
-
 
 if __name__ == '__main__':
-    p = Pool(3)
+    p = Pool(8)
+    start = time.time()
     p.map_async(multiple_processing_func, [key for key in processed_news_dict])
     p.close()
     p.join()
+    end = time.time()
+    print("All models fitted, total Time:" + str(end - start))
 
+    print("Dumping Pickle file.........\n")
     pickle_out = open("./data/intermediate/model.pickle", "wb")
     pickle.dump(fitted_models, pickle_out)
     pickle_out.close()
+    print("Finish dumping\n")
 
-# pickle_in = open("./data/intermediate/model.pickle", "rb")
-# lm = pickle.load(pickle_in)
-# print(lm.score('held', ['zealands', 'currency']))
 
 
