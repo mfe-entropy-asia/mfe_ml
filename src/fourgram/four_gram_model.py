c5ffa649daab233a2a496e01f32988dcaa50854e
yi liu <liuyi92815@gmail.com>
2019-04-04 22:31:52 +0800

    adding stop words and stemmer

--- src/fourgram/four_gram_model.py
+++ src/fourgram/four_gram_model.py
@@ -9,10 +9,12 @@ from nltk.lm import MLE
 import string
 import pickle
 import time
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 # from nltk import Text
 # from data_cleaner import DataCleaner
 # from nltk.util import pad_sequence
-from nltk.util import ngrams
+# from nltk.util import ngrams
 # from nltk import FreqDist
 
 
@@ -32,6 +34,7 @@ class FourGramModel:
         self.vocab = []
         self.lm = MLE(4)
         self.translator = str.maketrans('', '', string.punctuation)  # To get rid of the punctuations
+        self.stopWords = set(stopwords.words('english'))
         return
 
     def __call__(self):
@@ -46,6 +49,19 @@ class FourGramModel:
               "Time taking: " + str(end-start) + "\n"
               "===============================================\n")
 
+    def stop_words_and_stem(self, word_list):
+        """
+        This is the function which will remove the stop words and do the word stemming
+        :param word_list: a list of words to be processed
+        :return:
+        """
+        return_list = []
+        ps = PorterStemmer()
+        for w in word_list:
+            if w not in self.stopWords:
+                return_list.append(ps.stem(w))
+        return return_list
+
     def train_model(self):
         """
         Train the model with data one week before
@@ -55,8 +71,9 @@ class FourGramModel:
         training_text = []
         for idx in range(7):
             for content in self.input_data_frame[self.input_date - idx - 1]:
-                training_text += [word_tokenize(sentence.translate(self.translator))
+                training_text += [self.stop_words_and_stem(word_tokenize(sentence.translate(self.translator)))
                                   for sentence in sent_tokenize(content)]
+        print(training_text[1])
         # self.train, self.vocab = padded_everygram_pipeline(4, training_text)
         every_gram, vocab = padded_everygram_pipeline(4, training_text)  # This will generate unigram, bigram,
         # trigram and fourgram
